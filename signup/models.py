214625e3b70098a793941303f8c0732bc82c3db3
Nachiket Londhe <londhe.nachiket@gmail.com>
2017-06-15 00:07:54 -0400

    Bunch of midnight checkin

--- signup/models.py
+++ signup/models.py
@@ -9,6 +9,5 @@ class DraftPod(models.Model):
 class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    dci_number = models.CharField(max_length=15)
     draft_pod = models.ForeignKey(DraftPod, on_delete=models.CASCADE)
     
--- signup/views.py
+++ signup/views.py
@@ -3,7 +3,7 @@ from django.template.context import RequestContext
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import DraftPod
+from .models import DraftPod, Player
 
 def home(request):
     return render(request, 'signup/home.html')
@@ -17,10 +17,25 @@ def index(request):
 
 def register(request):
     if request.method == 'POST':
-        player = request.POST.get('player_name')
-        draft_date = request.POST.get('draft_date')
-        print(player)
-        print(draft_date)
-#        latest_available_pod = DraftPod.objects.last()
+        player_name = request.POST.get('player_name').split()
+        date = request.POST.get('draft_date')
+
+        try:
+            draft_pod = DraftPod.objects.get(draft_date=date).last()    # Get the last available for for the given date
+        except DraftPod.DoesNotExist:
+            draft_pod = None       # Create a new pod if one doesn't exist
+            
+        if draft_pod == None or draft_pod.player_count <= 0:
+            draft_pod = DraftPod(draft_date=date, player_count=7)
+        else:
+            player_count = draft_pod.player_count - 1
+            draft_pod.player_count = player_count
+
+        #Save the drfat pod
+        draft_pod.save()
+
+        player = Player(first_name=player_name[0], last_name=player_name[1], draft_pod=draft_pod)
+        player.save()
+        
     return render(request, 'signup/register.html')
 
